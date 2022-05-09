bb372ac8ed6b25622af1c57df71edc8de40df2fc
RudyK24 <rudra@ronin.co.in>
2022-05-09 13:28:34 +0530

    Update ui-root.js

--- src/react-components/ui-root.js
+++ src/react-components/ui-root.js
@@ -783,17 +783,17 @@ class UIRoot extends Component {
     });
   }
 
-  timePeriod = () => {
+  // timePeriod = () => {
 
-    var now = new Date();
-    if(now.getHours() <= 17 ){
-      console.log("4pm to 5pm is event time so you will be muted till then");
-    }
-    else{
-      return (<AudioPopoverContainer scene={this.props.scene} />);
-    }
+  //   var now = new Date();
+  //   if(now.getHours() <= 17 ){
+  //     console.log("4pm to 5pm is event time so you will be muted till then");
+  //   }
+  //   else{
+  //     return (<AudioPopoverContainer scene={this.props.scene} />);
+  //   }
 
-  };
+  // }
 
   onFocusChat = e => {
     this.setSidebar("chat", {
