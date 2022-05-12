5cd5701acbb286f93eaf22230438c689ecf8284a
Satish Gowda <satishgowda28@gmail.com>
2022-05-12 09:38:46 +0530

    click fix

--- src/react-components/room/CustomRoomBtn.js
+++ src/react-components/room/CustomRoomBtn.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./CustomRoomBtn.scss";
-const CustomRoomBtn = ({ onClickFn, icon, label }) => {
+const CustomRoomBtn = ({ onClick, icon, label }) => {
   return (
     <div>
-      <button className={styles.avatarBtn} onClick={onClickFn}>
+      <button className={styles.avatarBtn} onClick={onClick}>
         {icon}
         <label>{label}</label>
       </button>
