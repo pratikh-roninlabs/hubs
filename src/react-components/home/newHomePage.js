f952277d0db33d4c4838ccbf2ea1c99b4f8e83b3
Satish Gowda <satishgowda28@gmail.com>
2022-05-16 09:00:56 +0530

    home page wip

--- src/react-components/home/newHomePage.js
+++ src/react-components/home/newHomePage.js
@@ -1,20 +1,22 @@
 /* eslint-disable @calm/react-intl/missing-formatted-message */
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "../layout/Container";
 import BBLogo from "../../assets/images/hdfcbb/bouncebackbatch_logo.svg";
 import HDFCLogo from "../../assets/images/hdfcbb/hdfcLife.png";
-import speaker from "../../assets/images/hdfcbb/biswa-kalyan-rath-profile-1.jpg";
-import tw from "../../assets/images/hdfcbb/twiiter.svg";
-import li from "../../assets/images/hdfcbb/linkedin.svg";
-import yt from "../../assets/images/hdfcbb/yt.svg";
+import vibha from "../../assets/images/hdfcbb/vibha.jpg";
+import sunil from "../../assets/images/hdfcbb/suni.jpg";
 /* styles */
 import styles from "./newHomePage.scss";
-import { RoomEntryModal } from "../room/RoomEntryModal";
 
 const NewHomePage = () => {
+  const [tab, setTab] = useState("register");
+  const handleClick = elem => {
+    setTab(elem);
+    const domElem = document.querySelector(`#${elem}`);
+    domElem.scrollIntoView({ behavior: "smooth", block: "start" });
+  };
   return (
     <div>
-      <RoomEntryModal roomName={"test"} />
       <Container>
         <div>
           <div className={styles.hpHeader}>
@@ -29,176 +31,124 @@ const NewHomePage = () => {
           <div className={styles.bbbLogoWrapper}>
             <img src={BBLogo} alt={"BounceBack_logo"} />
           </div>
-          <div className={styles.hpHeroContent}>
-            <p>
-              A one-of-a kind <span>virtual convocation on the metaverse</span>
-              to celebrate the BounceBack batch of 2022.
-            </p>
-            <p>
-              Join in the fun on <span>May 15th, 4pm</span> onwards.
-            </p>
+          <div className={styles.tabsWrapper}>
+            <ul className={styles.tabs}>
+              <li className={tab === "register" && "active"} onClick={() => handleClick("register")}>
+                Register
+              </li>
+              <li className={tab === "speakers" && "active"} onClick={() => handleClick("speakers")}>
+                Speakers
+              </li>
+              <li className={tab === "highlights" && "active"} onClick={() => handleClick("highlights")}>
+                Highlights
+              </li>
+            </ul>
           </div>
-          <div className={styles.hpHeroVideo}>
-            <div className={styles.hpHeroVdoWrapper}>
-              <div>
-                <iframe
-                  width="100%"
-                  height="100%"
-                  src="https://www.youtube.com/embed/oi1ntQwBJ9Y"
-                  title="YouTube video player"
-                  frameborder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowfullscreen
-                />
+          <section id="register">
+            <div className={styles.hpHeroContent}>
+              <p>
+                A unique <span>virtual convocation on the Metaverse</span> to celebrate the BounceBack Batch of 2022.
+                Join the fun on
+                <span>22nd May, 4pm onwards</span>.
+              </p>
+            </div>
+            <div className={styles.hpHeroVideo}>
+              <div className={styles.hpHeroVdoWrapper}>
+                <div>
+                  <iframe
+                    width="100%"
+                    height="100%"
+                    src="https://www.youtube.com/embed/oi1ntQwBJ9Y"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                  />
+                </div>
+              </div>
+              <p>
+                This event is to honour all the students, teachers and family members of the BounceBack Batch. Register
+                now to take part in this exciting event.
+              </p>
+              <div className={styles.hpHeroRegister}>
+                <a href="#">Regitster for the event</a>
               </div>
             </div>
-            <p>
-              This event is to honor all the students, teachers and family members of the BounceBack Batch. Join us in
-              Join us in this exciting event which will be full of entertainment and inspiration!
-            </p>
-            <div className={styles.hpHeroRegister}>
-              <a href="#">Regitster for the event</a>
-            </div>
-          </div>
+          </section>
         </div>
       </Container>
-      <div className={styles.speakerWrapper}>
-        <h2 className={styles.hpSectionTitle}>Guest Speakers</h2>
-        <ul className={styles.speakerList}>
-          <li>
-            <div className={"speakerImg"}>
-              <img src={speaker} alt="" />
-            </div>
-            <div className={"speakerIntro"}>
-              <div>
-                <h3>Biswas Bist</h3>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis numquam repudiandae nobis
-                  voluptates rem eveniet maxime consectetur possimus, atque repellat quia, porro praesentium vel
-                  voluptatem quos nihil soluta dolorem eum.
-                </p>
-                <ul className="socialLink">
-                  <li>
-                    <a href="#">
-                      <img src={tw} alt="" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <img src={li} alt="" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <img src={yt} alt="" />
-                    </a>
-                  </li>
-                </ul>
+      <section id="speakers">
+        <div className={styles.speakerWrapper}>
+          <h2 className={styles.hpSectionTitle}>Guest Speakers</h2>
+          <ul className={styles.speakerList}>
+            <li>
+              <div className={"speakerImg"}>
+                <img src={vibha} alt="" />
               </div>
-            </div>
-          </li>
-          <li>
-            <div className={"speakerImg"}>
-              <img src={speaker} alt="" />
-            </div>
-            <div className={"speakerIntro"}>
-              <div>
-                <h3>Biswas Bist</h3>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis numquam repudiandae nobis
-                  voluptates rem eveniet maxime consectetur possimus, atque repellat quia, porro praesentium vel
-                  voluptatem quos nihil soluta dolorem eum.
-                </p>
-                <ul className="socialLink">
-                  <li>
-                    <a href="#">
-                      <img src={tw} alt="" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <img src={li} alt="" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <img src={yt} alt="" />
-                    </a>
-                  </li>
-                </ul>
+              <div className={"speakerIntro"}>
+                <div>
+                  <h3>Vibha Padalkar</h3>
+                  <p>
+                    CEO and MD of HDFC Life, Vibha Padalkar is an inspirational leader and recipient of the "Most
+                    Powerful Women in Business" award by Fortune International and Business Today for 4 consecutive
+                    years.
+                  </p>
+                </div>
               </div>
-            </div>
-          </li>
-          <li>
-            <div className={"speakerImg"}>
-              <img src={speaker} alt="" />
-            </div>
-            <div className={"speakerIntro"}>
-              <div>
-                <h3>Biswas Bist</h3>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis numquam repudiandae nobis
-                  voluptates rem eveniet maxime consectetur possimus, atque repellat quia, porro praesentium vel
-                  voluptatem quos nihil soluta dolorem eum.
-                </p>
-                <ul className="socialLink">
-                  <li>
-                    <a href="#">
-                      <img src={tw} alt="" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <img src={li} alt="" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <img src={yt} alt="" />
-                    </a>
-                  </li>
-                </ul>
+            </li>
+            <li>
+              <div className={"speakerImg"}>
+                <img src={sunil} alt="" />
               </div>
-            </div>
-          </li>
-          <li>
-            <div className={"speakerImg"}>
-              <img src={speaker} alt="" />
-            </div>
-            <div className={"speakerIntro"}>
-              <div>
-                <h3>Biswas Bist</h3>
-                <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis numquam repudiandae nobis
-                  voluptates rem eveniet maxime consectetur possimus, atque repellat quia, porro praesentium vel
-                  voluptatem quos nihil soluta dolorem eum.
-                </p>
-                <ul className="socialLink">
-                  <li>
-                    <a href="#">
-                      <img src={tw} alt="" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <img src={li} alt="" />
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <img src={yt} alt="" />
-                    </a>
-                  </li>
-                </ul>
+              <div className={"speakerIntro"}>
+                <div>
+                  <h3>Sunil Chhetri</h3>
+                  <p>
+                    Captain of the Indian National football team, Sunil Chhetri is a proud recipient of the Arjuna
+                    Award, the Padma Shri, the Khel Ratna and many more!
+                  </p>
+                </div>
               </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <div className={styles.hpEventList}>
-        <h2 className={styles.hpSectionTitle}>Event Hightlights</h2>
-        <div className={styles.hpSliders}>slider</div>
-      </div>
+            </li>
+            <li>
+              <div className={"speakerImg"}>
+                <img src={vibha} alt="" />
+              </div>
+              <div className={"speakerIntro"}>
+                <div>
+                  <h3>Biswas Bist</h3>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis numquam repudiandae nobis
+                    voluptates rem eveniet maxime consectetur possimus, atque repellat quia, porro praesentium vel
+                    voluptatem quos nihil soluta dolorem eum.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className={"speakerImg"}>
+                <img src={vibha} alt="" />
+              </div>
+              <div className={"speakerIntro"}>
+                <div>
+                  <h3>Biswas Bist</h3>
+                  <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis numquam repudiandae nobis
+                    voluptates rem eveniet maxime consectetur possimus, atque repellat quia, porro praesentium vel
+                    voluptatem quos nihil soluta dolorem eum.
+                  </p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section id="highlights">
+        <div className={styles.hpEventList}>
+          <h2 className={styles.hpSectionTitle}>Event Hightlights</h2>
+          <div className={styles.hpSliders}>slider</div>
+        </div>
+      </section>
       <Container>
         <div className={styles.hpRulestoParticipate}>
           <h2 className={styles.hpSectionTitle}>How to participate</h2>
@@ -216,14 +166,15 @@ const NewHomePage = () => {
       <div className={styles.footerWrapper}>
         <h2>Terms and Conditions - Privacy Policy</h2>
         <p>
-          HDFC Life Insurance Company Limited ("HDFC Life"). CIN: L65110MH2000PLC128245, IRDAI Registration No. 101.<br />
-          Registered Office: 13th Floor, Lodha Excelus, Apollo Mills Compound, N.M. Joshi Marg, Mahalaxmi, Mumbai - 400
-          011.<br />
+          <b>HDFC Life Insurance Company Limited ("HDFC Life")</b>. CIN: L65110MH2000PLC128245, IRDAI Registration No.
+          101.<br />
+          <b>Registered Office</b>: 13th Floor, Lodha Excelus, Apollo Mills Compound, N.M. Joshi Marg, Mahalaxmi, Mumbai
+          - 400 011.<br />
           Email: service@hdfclife.com,Tel No: 1860-267-9999. Available (Mon-Sat 10 am to 7 pm) Local charges apply. DO
           NOT prefix any country code e.g. +91 or 00, Website: www.hdfclife.com<br />
           The name/letters "HDFC" in the name/logo of the company belongs to Housing Development Finance Corporation
           Limited ("HDFC Limited") and is used by HDFC Life under an agreement entered into with HDFC Limited.<br />
-          ARN: MC/04/22/28457
+          ARN: INT/MC/05/22/28801
         </p>
       </div>
     </div>
