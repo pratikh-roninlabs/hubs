2344afb410850a6591f66352f0e1384815b024bb
unknown <pratik@ronin.co.in>
2022-05-21 08:35:57 +0530

    v1 setup

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ import { WrappedIntlProvider } from "./react-components/wrapped-intl-provider";
 import registerTelemetry from "./telemetry";
 import Store from "./storage/store";
 import "./utils/theme";
-// import NewHomePage from "./react-components/home/newHomePage";
-import NewHomePagePost from "./react-components/home/newHomePagePost";
+import NewHomePage from "./react-components/home/newHomePage";
+// import NewHomePagePost from "./react-components/home/newHomePagePost";
 import { AuthContextProvider } from "./react-components/auth/AuthContext";
 import "./react-components/styles/global.scss";
 import { ThemeProvider } from "./react-components/styles/theme";
-// import RoomTopBar from "./react-components/room/RoomTopBar";
 
 registerTelemetry("/home", "Hubs Home Page");
 
@@ -21,9 +20,8 @@ function Root() {
     <WrappedIntlProvider>
       <ThemeProvider store={store}>
         <AuthContextProvider store={store}>
-          {/* <RoomTopBar /> */}
-          {/* <NewHomePage /> */}
-          <NewHomePagePost />
+          <NewHomePage />
+          {/* <NewHomePagePost /> */}
         </AuthContextProvider>
       </ThemeProvider>
     </WrappedIntlProvider>
